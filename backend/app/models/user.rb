cb805687a3e67ce84fb5054566bf673aeefb5086
haruka tezuka <th_kiyose@outlook.com>
2022-06-08 12:08:36 +0900

    Userモデルのバリデーション設定

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_validation :email_downcase
+
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :email, presence: true, length: { maximum: 255}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :admin, presence: true
+
+  def email_downcase
+    self.email.downcase!
+  end
 end
