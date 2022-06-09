e5ca443bd4002f87df183848461bb442a88e9cea
haruka tezuka <th_kiyose@outlook.com>
2022-06-09 14:47:37 +0900

    ヘッダーコンポーネントの作成

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Header } from './parts/Header'
 
 export const Login = () => {
   return (
-    <>
-    </>
+    <Header />
   );
 };
---
+++ frontend/src/components/parts/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const style = {
+  width:"100%",
+  backgroundColor:"rgb(249, 254, 207)",
+  height:"50px",
+  position:"fixed"
+};
+
+export const Header = () => {
+  return (
+    <div style={style}>Header</div>
+  )
+};
