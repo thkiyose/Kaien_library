e5f07251be6dba606986b437b5bc5b0357b2dce0
haruka tezuka <th_kiyose@outlook.com>
2022-06-09 14:42:55 +0900

    背景色の設定、コンポーネント作成

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './components/Login'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path={"/"} />
-        <Route exact path={"/signup"} />
+        <Route exact path={"/"} element={<Login />} />
       </Routes>
      </BrowserRouter>
   );
