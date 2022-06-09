237e695b4fb7aab5607dd375a10b0485a540a91f
haruka tezuka <th_kiyose@outlook.com>
2022-06-09 12:12:21 +0900

    各種アクションの定義

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def login!
+    session[:user_id] = @user.id
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def create
-    user = User.new
-    if user.save
-      render json: { status:"SUCCESS", data: user}
+    @user = User.new
+    if @user.save
+      render json: { status:"SUCCESS", data: @user}
     else
-      render json: { user.errors, status: 422 }
+      render json: { @user.errors, status: 500 }
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
