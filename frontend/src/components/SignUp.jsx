e7f6c78537e85733f04759195ff1772e53a1bc6d
haruka tezuka <th_kiyose@outlook.com>
2022-06-12 12:48:28 +0900

    react-hook-formを導入しログインフォームのコンポーネントをそれに合わせて修正

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import ReactDOM from "react-dom";
 import { Header } from './parts/Header';
 import { Wrapper } from './parts/Wrapper';
 import { useNavigate } from 'react-router-dom';
 import { createUser } from '../lib/api/user';
+import { useForm } from 'react-hook-form';
 
 export const SignUp = () => {
   const [value,setValue] = useState({});
   const navigate = useNavigate();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(watch("name"));
 
   const handleChange = (e) => {
     setValue({
@@ -15,35 +20,30 @@ export const SignUp = () => {
     })
   };
 
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-    try {
-      await createUser({user: value});
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
   return (
     <>
       <Header />
       <Wrapper>
-        <form onSubmit={(e) => handleSubmit(e)}>
+        <form onSubmit={handleSubmit(async (data) => await createUser({user: data}))}>
           <div>
             <label>名前</label>
-            <input type="text" name="name" id="name" onChange={(e) => handleChange(e)} value={value.name}/>
+            <input type="text" name="name" {...register("name", { required: true })}/>
+            {errors.name && <span>名前を入力して下さい。</span>}
           </div>
           <div>
             <label>メールアドレス</label>
-            <input type="text" name="email" id="email" onChange={(e) => handleChange(e)} value={value.email}/>
+            <input type="text" name="email" {...register("email", { required: true })}/>
+            {errors.email && <span>メールアドレスを入力して下さい。</span>}
           </div>
           <div>
             <label>パスワード</label>
-            <input type="text" name="password" id="password" onChange={(e) => handleChange(e)} value={value.password}/>
+            <input type="text" name="password" {...register("password", { required: true })}/>
+            {errors.password && <span>パスワードを入力して下さい。</span>}
           </div>
           <div>
             <label>パスワード(確認)</label>
-            <input type="text" name="password_confirmation" id="password_confirmation" onChange={(e) => handleChange(e)} value={value.password_confirmation}/>
+            <input type="text" name="password_confirmation" {...register("password_confirmation", { required: true })}/>
+            {errors.password_confirmation && <span>同一のパスワードを再度入力して下さい。</span>}
           </div>
           <input value="アカウント作成" type="submit" />
         </form>
