b587b02df4db15220e600c979ebb5470849b11ab
haruka tezuka <th_kiyose@outlook.com>
2022-06-13 10:42:49 +0900

    CSRF対策を設定

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  before_action :check_xhr_header
+
   def login!
     session[:user_id] = @user.id
   end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+
+  def check_xhr_header
+    return if request.xhr?
+
+    render json: { error: 'forbidden' }, status: :forbidden
   end
 end
