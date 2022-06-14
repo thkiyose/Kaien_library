7893ebb3fb3e9a41796e2bb539f894441ad7c1aa
haruka tezuka <th_kiyose@outlook.com>
2022-06-14 10:37:26 +0900

    devise用のコントローラーを作成

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
+  skip_before_action :verify_authenticity_token
+  helper_method :current_user, :user_signed_in?
 end
