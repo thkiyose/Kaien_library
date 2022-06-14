7c99e94cf71c025b3f9b6dafd88a558790f5fed2
haruka tezuka <th_kiyose@outlook.com>
2022-06-14 11:35:12 +0900

    registrationsコントローラーのcreateアクションを定義

--- backend/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::Auth::RegistrationsController < ApplicationController
+  def create
+    @user = User.new(sign_up_params)
+    if @user.save!
+      render json: { status:"SUCCESS", data: @user}
+    else
+      render json: { status: 500 }
+    end
+  end
+
   private
 
   def sign_up_params
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::BASE
+class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
   helper_method :current_user, :user_signed_in?
