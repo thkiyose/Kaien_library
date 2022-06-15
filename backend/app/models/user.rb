e26cd3be0959ae9fef9668d71f4b59d9178ed8ea
haruka tezuka <th_kiyose@outlook.com>
2022-06-15 14:12:56 +0900

    nameとemailにdb制約を再設定、devise導入時に消えていたadminカラムを追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :validatable
   include DeviseTokenAuth::Concerns::User
+  before_validation :email_downcase
+
+  validates :name, presence: true, length: { maximum: 15 }
 end
