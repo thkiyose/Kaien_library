e6cbd2b7fbeef8aa4fcf6f64a467f832de27020b
haruka tezuka <th_kiyose@outlook.com>
2022-06-15 11:02:04 +0900

    ログアウト用の関数を定義

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,6 +1,14 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../App';
+import { signOut } from '../lib/api/session';
 
 export const LogOut = () => {
-  return <p>aaa</p>
+  const handleLogOut = async() => {
+    try {
+      const res = await signOut();
+    } catch(e) {
+      console.log(e);
+    }
+  };
+  return <button onClick={handleLogOut}>ログアウト</button>
 };
