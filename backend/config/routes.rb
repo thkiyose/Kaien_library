33b52cc2fa44386c137756e1c627994211f061e6
haruka tezuka <th_kiyose@outlook.com>
2022-06-15 15:20:22 +0900

    ルーティングとコントローラー名のミスを修正

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'users/show'
+    end
+  end
+  get 'users/show'
+  namespace :api do
+    namespace :v1 do
 
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks], controllers: {
         registrations: 'api/v1/auth/registrations'
       }
-      resources :user, except: [:create,:index]
+      resources :users, except: [:create,:index]
 
       namespace :auth do
         resources :sessions, only: %i[index]
