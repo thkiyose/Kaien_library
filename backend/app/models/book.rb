0d80c48b88b158bb8414f5dc5fce892f05d00353
haruka tezuka <th_kiyose@outlook.com>
2022-06-20 13:42:52 +0900

    Bookモデルのmodel specを記述、実施

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -2,12 +2,12 @@ class Book < ApplicationRecord
   belongs_to :category
   belongs_to :location
 
-  validates :isbn, length: { minimum:10, maximum:13 }
+  validates :isbn, length: { minimum:10, maximum:13 }, allow_blank: true
   validates :title, presence: true, length: { maximum:255 }
   validates :author ,presence: true, length: { maximum:255 }
   validates :category_id, presence: true
   validates :published_year, presence: true, length: { minimum: 4, maximum: 4 }
   validates :description, presence: true
-  validates :location, presence: true
+  validates :location_id, presence: true
   validates :version, length: { maximum: 3 }
 end
--- backend/spec/models/book_spec.rb
+++ backend/spec/models/book_spec.rb
@@ -1,9 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  before do
+    Category.create(id:1,category:"文芸")
+    Location.create(id:1, location:"1F本棚")
+  end
+
   describe "Book Registration" do
-    it "is invalid with wrong number of digits of isbn  " do
-      book = FactoryBot.build(:book, isbn:"000000000000000033330")
+    it "is invalid with wrong number of digits of isbn" do
+      book1 = FactoryBot.build(:book, isbn:"0000")
+      book2 = FactoryBot.build(:book, isbn:"000000000000000")
+      expect(book1).not_to be_valid
+      expect(book2).not_to be_valid
+    end
+
+    it "is invalid without a title" do
+      book = FactoryBot.build(:book, title:"")
+      expect(book).not_to be_valid
+    end
+
+    it "is invalid with a title that is too long" do
+      book = FactoryBot.build(:book, title:"a" *256)
+      expect(book).not_to be_valid
+    end
+
+    it "is invalid without an author name" do
+      book = FactoryBot.build(:book, author:"")
+      expect(book).not_to be_valid
+    end
+
+    it "is invalid with an author name that is too long" do
+      book = FactoryBot.build(:book, author:"a" *256)
+      expect(book).not_to be_valid
+    end
+
+    it "is invalid without a category id" do
+      book = FactoryBot.build(:book, category_id:"")
+      expect(book).not_to be_valid
+    end
+
+    it "is invalid with a published_year not 4 letters" do
+      book1 = FactoryBot.build(:book, published_year:"123")
+      book2 = FactoryBot.build(:book, published_year:"12345")
+      expect(book1).not_to be_valid
+      expect(book2).not_to be_valid
+    end
+
+    it "is invalid without a description" do
+      book = FactoryBot.build(:book, description:"")
+      expect(book).not_to be_valid
+    end
+
+    it "is invalid without a location id" do
+      book = FactoryBot.build(:book, location_id:"")
+      expect(book).not_to be_valid
+    end
+
+
+    it "is invalid without a version more than 3 digits" do
+      book = FactoryBot.build(:book, version:"1234")
       expect(book).not_to be_valid
     end
   end
