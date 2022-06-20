56b52072dcc6b1a360c47f3cf1c28a0cf26b0ac5
haruka tezuka <th_kiyose@outlook.com>
2022-06-20 11:30:32 +0900

    シードデータを作成

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#カテゴリ用データ
+Category.create(id:1, category:"文芸")
+
+#場所用データ
+Location.create(id:1, location:"1F本棚")
