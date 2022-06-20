dad37daeedc2466e856f9befded043744e8cc560
haruka tezuka <th_kiyose@outlook.com>
2022-06-20 14:14:16 +0900

    管理者メニュー画面を暫定的に作成

--- frontend/src/components/AdminMenu.jsx
+++ frontend/src/components/AdminMenu.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Header } from './parts/Header'
+import { Wrapper } from './parts/Wrapper'
+
+const style= {
+  listStyle:"none"
+};
+const styledTitle= {
+  fontSize: "1.5rem",
+  fontWeight:"lighter"
+};
 
 export const AdminMenu = () => {
-  return <p>test component</p>
+  return(
+    <>
+      <Header />
+      <Wrapper>
+        <div style={style}>
+          <h1 style={styledTitle}>管理者メニュー</h1>
+          <ul>
+            <li><p>書籍の登録</p></li>
+          </ul>
+        </div>
+      </Wrapper>
+    </>
+  );
 };
