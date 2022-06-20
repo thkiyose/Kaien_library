c33c0d38003e7483cccbb851aa596d68cd7e59ac
haruka tezuka <th_kiyose@outlook.com>
2022-06-20 14:22:51 +0900

    本の登録画面とフォームのコンポーネントを作成

---
+++ frontend/src/components/RegisterBook.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BookForm } from ' ./parts/BookForm'
+
+export const RegisterBook = () => {
+  return (
+    <>
+      <BookForm />
+    </>
+  );
+};
---
+++ frontend/src/components/parts/BookForm.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const BookForm = () => {
+  return <p>form</p>
+};
