45f0bddf7660db5d1f38396c96836e96863838d1
haruka tezuka <th_kiyose@outlook.com>
2022-06-20 11:43:21 +0900

    アソシエーション設定

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,14 @@
 #カテゴリ用データ
-Category.create(id:1, category:"文芸")
+Category.first_or_create(id:1, category:"文芸")
 
 #場所用データ
-Location.create(id:1, location:"1F本棚")
+Location.first_or_create(id:1, location:"1F本棚")
+
+Book.create(
+  title:"テスト",
+  author:"夏目漱石",
+  category_id:1,
+  published_date:"2022-06-01",
+  description:"テストデータ",
+  location_id: 1
+)
