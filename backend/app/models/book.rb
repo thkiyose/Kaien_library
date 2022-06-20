c68a4914e4930fa785df804efd6b19d6fb8398cb
haruka tezuka <th_kiyose@outlook.com>
2022-06-20 12:13:16 +0900

    Bookモデルのバリデーションを設定

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
   belongs_to :category
   belongs_to :location
+
+  validates :isbn, length: { minimum:10, maximum:13 }
+  validates :title, presence: true, length: { maximum:255 }
+  validates :author ,presence: true, length: { maximum:255 }
+  validates :category_id, presence: true
+  validates :published_year, presence: true, length: { minimum: 4, maximum: 4 }
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :version, length: { maximum: 3 }
 end
