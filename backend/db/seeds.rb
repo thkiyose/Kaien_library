16cb37a0dcea4b291371bf36880de8366316320e
haruka tezuka <th_kiyose@outlook.com>
2022-06-22 15:08:53 +0900

    CategoryとLocatitonのシードデータを追加

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,14 +1,25 @@
 #カテゴリ用データ
-Category.first_or_create(id:1, category:"文芸")
+Category.find_or_create_by(id:1, category:"文学/評論")
+Category.find_or_create_by(id:2, category:"ノンフィクション")
+Category.find_or_create_by(id:3, category:"ビジネス/経済")
+Category.find_or_create_by(id:4, category:"地理/歴史")
+Category.find_or_create_by(id:5, category:"政治/社会")
+Category.find_or_create_by(id:6, category:"芸能/エンターテインメント")
+Category.find_or_create_by(id:7, category:"アート/建築/デザイン")
+Category.find_or_create_by(id:8, category:"人文/思想/宗教")
+Category.find_or_create_by(id:9, category:"暮らし/健康/料理")
+Category.find_or_create_by(id:10, category:"サイエンス/テクノロジー")
+Category.find_or_create_by(id:11, category:"趣味/実用")
+Category.find_or_create_by(id:12, category:"教育")
+Category.find_or_create_by(id:13, category:"スポーツ/アウトドア")
+Category.find_or_create_by(id:14, category:"辞典/年鑑")
+Category.find_or_create_by(id:15, category:"音楽")
+Category.find_or_create_by(id:16, category:"旅行")
+Category.find_or_create_by(id:17, category:"絵本/児童書")
+Category.find_or_create_by(id:18, category:"コミック")
 
 #場所用データ
-Location.first_or_create(id:1, location:"1F本棚")
-
-Book.create(
-  title:"テスト",
-  author:"夏目漱石",
-  category_id:1,
-  published_year:"2022",
-  description:"テストデータ",
-  location_id: 1
-)
+Location.find_or_create_by(id:1, location:"2Fオフィス本棚")
+Location.find_or_create_by(id:2, location:"2Fエレベーターホール本棚")
+Location.find_or_create_by(id:3, location:"3F資料室")
+Location.find_or_create_by(id:4, location:"4F物置")
