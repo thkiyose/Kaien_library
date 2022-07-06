6458528fce38513e2228bba663586526460dd430
haruka tezuka <th_kiyose@outlook.com>
2022-07-06 10:49:48 +0900

    LendingのModelSpec用のファイルを作成

--- backend/spec/models/lending_spec.rb
+++ backend/spec/models/lending_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Lending, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Category.create(id:1,category:"文芸")
+    Location.create(id:1, location:"1F本棚")
+    book = FactoryBot.build(:book)
+  end
+
+  describe "Create lending" do
+    it "is invalid with wrong number of digits of isbn" do
+    end
+  end
 end
