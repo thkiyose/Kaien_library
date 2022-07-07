e7d2d5f5877f2caabea3be4d7b65e2071d9e82bb
haruka tezuka <th_kiyose@outlook.com>
2022-07-07 14:07:09 +0900

    lendingsコントローラのcreateアクションを定義)

--- backend/app/controllers/api/v1/lendings_controller.rb
+++ backend/app/controllers/api/v1/lendings_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::LendingsController < ApplicationController
   def create
+    lending = Lending.new(lending_params)
+    if lending.save!
+      render json: { status:"SUCCESS"}
+    else
+      render json:  lending.errors, status: 422
+    end
+  end
+
+  private
+
+  def lending_params
+    params.require(:lending).permit(:book_id,:start_date,:expiry_date)
   end
 end
