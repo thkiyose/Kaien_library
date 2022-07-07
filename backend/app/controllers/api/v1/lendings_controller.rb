41c85323c65ababe77f46afb1b9e56791f0e2104
haruka tezuka <th_kiyose@outlook.com>
2022-07-07 14:57:58 +0900

    貸出中の書籍が貸出操作されることがないようlendingsコントローラーに分岐を追加

--- backend/app/controllers/api/v1/lendings_controller.rb
+++ backend/app/controllers/api/v1/lendings_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::LendingsController < ApplicationController
   def create
     lending = Lending.new(lending_params)
-    if lending.save!
-      render json: { status:"SUCCESS"}
+    if Book.find_by(id: params[:book_id]).is_lent == false
+      if lending.save!
+        render json: { status:"SUCCESS"}
+      else
+        render json:  lending.errors, status: 422
+      end
     else
-      render json:  lending.errors, status: 422
+      render json: { message: "この書籍は貸出中です。"}
     end
   end
 
