28b51f582d224a3924849c9f1a49a64d6c1bfbaa
haruka tezuka <th_kiyose@outlook.com>
2022-07-07 14:00:33 +0900

    スタイルを設定、借りる期間の起点を当日に固定

--- frontend/src/components/Lending.jsx
+++ frontend/src/components/Lending.jsx
@@ -7,23 +7,74 @@ import Color from './parts/Color';
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
-import { format, addDays, addMonths, eachDayOfInterval } from 'date-fns';
+import { format, addMonths } from 'date-fns';
 import ja from 'date-fns/locale/ja';
 
 const BackButton = styled.button`
   outline: 0;
+  display:block;
   background: ${Color.primary};
   font-size: 0.8rem;
   border: 0;
   padding: 5px 15px;
   color: #FFFFFF;
   cursor: pointer;
+  margin-bottom:10px;
 `
+const Calendar = styled.div`
+  float:left;
+  padding-bottom: 10px;
+`
+
+const Detail = styled.div`
+  float:right;
+  width: 50%;
+  height: 300px;
+  table {
+    width: 100%;
+    border-collapse:  collapse;
+  }
+  td {
+    background-color: white;
+    padding: 10px;
+  }
+  th {
+    background-color: ${Color.text};
+    padding: 10px;
+    font-weight: lighter;
+  }
+`
+const Rent = styled.button`
+  padding: 30px;
+  float: left;
+  margin: 70px 85px;
+  background-color: ${Color.primary};
+  color : white;
+  border: none;
+  font-size: 1.3rem;
+  cursor: pointer;
+  border-bottom:4px solid rgba(0,0,0,0.2);
+  :hover {
+    background-color: #0c797a;
+  }
+  :active {
+  	-webkit-transform: translate(0,2px);
+  	-moz-transform: translate(0,4px);
+  	transform: translate(0,4px);
+  	border-bottom:none;
+	}
+`
+
 const EmptyGuide = styled.div`
   margin: 20px auto;
   background-color:${Color.text};
   padding: 20px;
   text-align: center;
+`
+const ClearFix = styled.div`
+  content: "";
+  display: block;
+  clear: both;
 `
 
 export const Lending = () => {
@@ -39,9 +90,15 @@ export const Lending = () => {
   });
 
   const handleSelect = (item) => {
-    const interval = (item.selection.startDate - item.selection.endDate) / 86400000;
-    if (interval > -15) {
-      setState({ ...state, ...item })
+    const interval = (new Date() - item.selection.endDate) / 86400000;
+    if (interval > -14) {
+      setState({
+        selection: {
+          startDate: new Date(),
+          endDate: item.selection.endDate,
+          key: 'selection'
+        }
+      });
     };
   };
   return (
@@ -50,7 +107,7 @@ export const Lending = () => {
         <BackButton onClick={() =>{navigate(-1)}}>&lt; 戻る</BackButton>
         {bookId ?
           <>
-            <div>
+            <Calendar>
             <DateRange
               editableDateInputs={true}
               months={1}
@@ -64,12 +121,24 @@ export const Lending = () => {
               dateDisplayFormat={"yyyy/MM/dd"}
               monthDisplayFormat={"yyyy年MMM"}
               showDateDisplay={false}
+
             />
-            </div>
-            <div>
-              <p>{format(state.selection.startDate, 'yyyy-MM-dd')}</p>
-              <p>{format(state.selection.endDate, 'yyyy-MM-dd')}</p>
-            </div>
+            </Calendar>
+            <Detail>
+              <p>本を借りる期間を選択して下さい。（最大2週間)</p>
+              <table>
+                <tbody>
+                  <tr>
+                    <th>開始日</th><td>{format(state.selection.startDate, 'yyyy-MM-dd')}</td>
+                  </tr>
+                  <tr>
+                    <th>返却期限日</th><td>{format(state.selection.endDate, 'yyyy-MM-dd')}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <Rent>この期間で借りる</Rent>
+            </Detail>
+            <ClearFix />
           </>
         :
           <EmptyGuide>書籍の情報を取得できません。書籍詳細画面からレンタル操作を行って下さい。</EmptyGuide>
