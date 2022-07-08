56495dad902df6e3be18e62fe873f7027c748d7b
haruka tezuka <th_kiyose@outlook.com>
2022-07-08 12:26:56 +0900

    レンタル一覧画面からlendings/showアクションにアクセスしレンタル一覧データを取得するするようuseEffectと関数を設定

--- frontend/src/components/MyPageLendings.jsx
+++ frontend/src/components/MyPageLendings.jsx
@@ -1,7 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import styled from "styled-components";
 import Color from './parts/Color';
 import { Context } from '../App';
+import { fetchLendings } from '../lib/api/lending';
 
 const Title = styled.h1`
   font-weight: lighter;
@@ -9,7 +10,15 @@ const Title = styled.h1`
 `
 export const MyPageLendings = () => {
   const { currentUser } = useContext(Context);
+  const [ isLoading, setIsLoading ] = useState(true);
 
+  const handleFetchLendings = async() => {
+    const res = await fetchLendings(currentUser.id);
+    console.log(res);
+    setIsLoading(true);
+  };
+
+  useEffect(() => {handleFetchLendings()},[]);
   return (
     <>
       <Title>レンタル/予約一覧</Title>
