0acaca307853b8049a07b237b97e9d780f4559b5
haruka tezuka <th_kiyose@outlook.com>
2022-07-08 11:48:05 +0900

    マイページをサイドバーとメインによる構造に変更

--- frontend/src/components/MyPage.jsx
+++ frontend/src/components/MyPage.jsx
@@ -1,6 +1,22 @@
 import React, { useContext } from 'react';
 import { Wrapper } from './parts/Wrapper';
 import { Context } from '../App';
+import { Outlet } from 'react-router-dom'
+import styled from "styled-components";
+import Color from './parts/Color';
+
+const SideBar = styled.div`
+  float: left;
+`
+const Main = styled.div`
+  float: right;
+`
+
+const ClearFix = styled.div`
+  content: "";
+  display: block;
+  clear: both;
+`
 
 export const MyPage = () => {
   const { currentUser } = useContext(Context);
@@ -9,9 +25,15 @@ export const MyPage = () => {
     <>
     <Wrapper width={"800px"}>
       { currentUser ?
-        <div>
-          <p>{currentUser.name}のマイページ</p>
-        </div>
+        <>
+          <SideBar>
+            <p>{currentUser.name}のマイページ</p>
+          </SideBar>
+          <Main>
+            <Outlet />
+          </Main>
+          <ClearFix />
+        </>
         : <p>ユーザーが見つかりませんでした。</p> }
     </Wrapper>
     </>
