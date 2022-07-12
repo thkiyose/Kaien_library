30e2e09b0a8790796c05afea977b6d801fab5e19
haruka tezuka <th_kiyose@outlook.com>
2022-07-12 11:30:26 +0900

    返却後の遷移先コンポーネントを作成

---
+++ frontend/src/components/ThankYouForLending.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components";
+import { Wrapper } from './parts/Wrapper';
+
+export const ThankYouForLending = () => {
+  return (
+    <>
+      <Wrapper width="500px">
+        ご利用ありがとうございました。
+      </Wrapper>
+    </>
+  );
+};
