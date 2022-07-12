59a961b00fbdb05e9037708d3543a05dd8cf292a
haruka tezuka <th_kiyose@outlook.com>
2022-07-12 14:22:37 +0900

    本の削除アクションで貸出中は論理削除を実行出来ないよう分岐を追加

--- backend/app/controllers/api/v1/books_controller.rb
+++ backend/app/controllers/api/v1/books_controller.rb
@@ -64,7 +64,11 @@ class Api::V1::BooksController < ApplicationController
 
   def delete_book
     book = Book.find_by(id: params[:id])
-    book.update(deleted:true)
+    if book.is_lent == false
+      book.update(deleted:true)
+    else
+      render json: {message:"貸出中につき削除出来ません。"}
+    end
   end
 
   private
