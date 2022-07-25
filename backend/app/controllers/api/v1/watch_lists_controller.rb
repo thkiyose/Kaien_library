22ea4c12fe2eba965b2c23b41cc331a818666fda
haruka tezuka <th_kiyose@outlook.com>
2022-07-25 14:13:17 +0900

    watch_listsコントローラを作成,createアクションを定義

---
+++ backend/app/controllers/api/v1/watch_lists_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::WatchListsController < ApplicationController
+  def create
+    user = User.find_by(id: params[:id])
+    watch_list = ser.watch_lists.build(book_id: params[:book_id])
+    if watch_list.save
+      render json: { status: "SUCCESS" }
+    else
+      render json: watch_list.errors, status: 422
+    end
+  end
+end
