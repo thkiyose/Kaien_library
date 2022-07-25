736f67e815a8c9bd8a18aa36d4206dec351bc23c
haruka tezuka <th_kiyose@outlook.com>
2022-07-25 12:31:42 +0900

    DB制約をテストするためModelSpecを実施

--- backend/spec/models/watch_list_spec.rb
+++ backend/spec/models/watch_list_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe WatchList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    Category.create(id:1, category:"文芸")
+    Location.create(id:1, location:"1F本棚")
+    @book = FactoryBot.create(:book)
+  end
+  describe "Add a book to watchlist" do
+    it "raises db error with duplicated pairs of user and book" do
+      list1 = WatchList.create(user_id:@user.id, book_id: @book.id)
+      expect(list1).to be_valid
+      expect do
+        WatchList.create(user_id:@user.id, book_id: @book.id)
+      end.to raise_error
+    end
+  end
 end
