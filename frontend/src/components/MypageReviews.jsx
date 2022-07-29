030714f6db9ac2ec129943f58a74725210e7a2ef
haruka tezuka <th_kiyose@outlook.com>
2022-07-29 14:11:04 +0900

    マイページのレビュー一覧用のコンポーネントを作成

---
+++ frontend/src/components/MypageReviews.jsx
@@ -0,0 +1,12 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import styled from "styled-components";
+import Color from './parts/Color';
+import { Context } from '../App';
+import { Modal } from './parts/Modal';
+
+const MypageReviews = () => {
+  return (
+    <p>test</p>
+  );
+}
