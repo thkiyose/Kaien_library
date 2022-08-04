911320b809e671d6a899e7ff1934c3e28d2b064d
haruka tezuka <th_kiyose@outlook.com>
2022-08-04 12:30:39 +0900

    本登録画面に戻るボタンを追加

--- frontend/src/components/admin/RegisterBook.jsx
+++ frontend/src/components/admin/RegisterBook.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
+import Color from '../parts/Color';
 import { Wrapper } from '../parts/Wrapper';
 import { BookForm } from '../parts/BookForm';
 
 export const RegisterBook = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <Wrapper width={"800px"}>
+        <BackButton onClick={() =>{navigate(-1)}}>&lt; 戻る</BackButton>
         <BookForm />
       </Wrapper>
     </>
   );
 };
+
+const BackButton = styled.button`
+  outline: 0;
+  background: ${Color.primary};
+  font-size: 0.8rem;
+  border: 0;
+  padding: 5px 15px;
+  color: #FFFFFF;
+  cursor: pointer;
+`
