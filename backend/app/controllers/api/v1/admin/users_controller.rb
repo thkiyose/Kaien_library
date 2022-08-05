914529c5617c9255b2f0cc44b158c4effbc7cad0
haruka tezuka <th_kiyose@outlook.com>
2022-08-05 14:22:40 +0900

    users/destroyの削除の仕組みを物理削除に変更

--- backend/app/controllers/api/v1/admin/users_controller.rb
+++ backend/app/controllers/api/v1/admin/users_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Admin::UsersController < ApplicationController
   def index
-    users = User.where(deleted: false).map{|user|{ id: user.id, name: user.name, email: user.email, admin: user.admin }}
+    users = User.all.map{|user|{ id: user.id, name: user.name, email: user.email, admin: user.admin }}
     render json: { users: users }
   end
 
   def destroy
     user = User.find_by(id:params[:id])
-    if user.update!(deleted:true, uid: nil, email:nil, name:"退会済みユーザー", encrypted_password: nil)
+    if user.destroy
       render json: {status:"SUCCESS"}
     end
   end
