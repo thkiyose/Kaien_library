fe9f700d915c1df54b94c8e3de3eb9009571b9c6
haruka tezuka <th_kiyose@outlook.com>
2022-08-15 11:00:41 +0900

    サイドバーに予約データ管理画面へのリンクを追加

--- frontend/src/components/admin/AdminData.jsx
+++ frontend/src/components/admin/AdminData.jsx
@@ -13,6 +13,7 @@ export const AdminData = () => {
             <li><Link to="books">書籍データ一覧</Link></li>
             <li><Link to="users">ユーザーデータ一覧</Link></li>
             <li><Link to="lendings">貸出データ一覧</Link></li>
+            <li><Link to="reservations">予約データ一覧</Link></li>
           </ul>
         </SideBar>
         <Main>
