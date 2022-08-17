d11ce1f3c93b029db5a05bcaec3b11e1fdb10450
haruka tezuka <th_kiyose@outlook.com>
2022-08-17 13:50:50 +0900

    レビュー検索用アクションを定義

--- backend/app/controllers/api/v1/admin/reviews_controller.rb
+++ backend/app/controllers/api/v1/admin/reviews_controller.rb
@@ -12,4 +12,17 @@ class Api::V1::Admin::ReviewsController < ApplicationController
       render json: review.errors
     end
   end
+
+  def search
+    reviews = Review.joins(:book,:user).order(created_at: :desc)
+    .search_with_book_title(params[:title])
+    # .search_with_user_name(params[:user_name])
+    # .search_with_user_email(params[:user_email])
+    # .search_with_user_id(params[:user_id])
+    # .search_with_comment(params[:comment])
+    # .search_with_start_date(params[:start_date][0],params[:start_date][1])
+    # .search_with_finished_at(params[:finished_at][0],params[:finished_at][1])
+    .select(:id,:title,:user_id,:book_id,:rating,:comment,:name,:email)
+    render json: { reviews: reviews }
+  end
 end
