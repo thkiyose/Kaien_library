3a19b72135aff36e70efb0461f7b733487fa6719
haruka tezuka <th_kiyose@outlook.com>
2022-08-22 11:33:08 +0900

    登録結果画面の大まかな構成を作成

--- frontend/src/components/admin/ImportResult.jsx
+++ frontend/src/components/admin/ImportResult.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Wrapper } from '../parts/Wrapper';
 import { useLocation } from 'react-router-dom';
 
 export const ImportResult = () => {
   const location = useLocation();
   const result = location.state;
-  console.log(result);
   return (
     <>
       <Wrapper width={"800px"}>
         <h1>インポート結果</h1>
-        <p></p>
+        <table>
+          <thead>
+            <tr>
+              <th></th><th>タイトル</th><th>結果</th>
+            </tr>
+          </thead>
+          <tbody>
+            {result.map((result,index) => {
+                return (
+                  <tr>
+                    <td>{index}</td>
+                    <td>{result.title ? result.title : "タイトルがありません"}</td>
+                    <td>{ result.status === "SUCCESS" && "登録成功"}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
       </Wrapper>
     </>
   );
